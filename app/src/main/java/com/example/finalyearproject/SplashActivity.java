a11076dcf4003edfcd41141cbeaa1ea6a90b525c
unknown <melkywafula29@gmail.com>
2025-06-16 23:17:43 +0300

    added a splashscreen

---
+++ app/src/main/java/com/example/finalyearproject/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.example.finalyearproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private TextView progressText;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        progressBar = findViewById(R.id.splashProgressBar);
+        progressText = findViewById(R.id.progressPercentage);
+
+        new Thread(() -> {
+            while (progressStatus < 100) {
+                progressStatus += 1;
+                handler.post(() -> {
+                    progressBar.setProgress(progressStatus);
+                    progressText.setText(progressStatus + " %");
+                });
+                try {
+                    Thread.sleep(30); // adjust speed of progress
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // When done, launch RegisterActivity
+            startActivity(new Intent(SplashActivity.this, RegisterActivity.class));
+            finish();
+        }).start();
+    }
+}
